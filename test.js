458a3f11a8522cb3cd93ab44ba087d9736d23231
Lonkapardhu <pardhugadichannel99@gmail.com>
2023-08-13 23:55:03 +0530

    responsiveness (media queries)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const containers = document.querySelectorAll('.container');
-
+    // This event listener waits for the DOM (Document Object Model) to be fully loaded before executing the provided callback function
     containers.forEach(container => {
         container.addEventListener('click', () => {
             alert('Clicked');
