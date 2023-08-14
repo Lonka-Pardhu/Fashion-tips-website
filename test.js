8ea7caf25033761cee614294c08d7242fa8804af
Lonkapardhu <pardhugadichannel99@gmail.com>
2023-08-14 22:22:44 +0530

    media queries for min-width-1024px

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const containers = document.querySelectorAll('.container');
-    // This event listener waits for the DOM (Document Object Model) to be fully loaded before executing the provided callback function
-    containers.forEach(container => {
-        container.addEventListener('click', () => {
-            alert('Clicked');
-        });
-    });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const containers = document.querySelectorAll('.container');
+//     // This event listener waits for the DOM (Document Object Model) to be fully loaded before executing the provided callback function
+//     containers.forEach(container => {
+//         container.addEventListener('click', () => {
+//             alert('Clicked');
+//         });
+//     });
+// });
